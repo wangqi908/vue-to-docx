d86bf751f4a548c8fe386380ee5c7717c23227f2
wangqi <wangqi908@hotmail.com>
2021-01-30 09:40:59 +0800

    实现word插入图片并且控制图片尺寸功能

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,26 @@ export default {
   setup () {
     function down () {
       const data = {
-        first_name: 'John',
-        last_name: 'Doe',
+        firstName: 'John',
+        lastName: 'Doe',
         phone: '0652455478',
         description: 'New Website',
+        avatar: require('../static/avatar.jpg'),
         list: [
           {
             name: '张三',
-            hobby: '抽烟'
+            hobby: '抽烟',
+            image: require('../static/aa.jpg')
           },
           {
             name: '李四',
-            hobby: '喝酒'
+            hobby: '喝酒',
+            image: require('../static/bb.jpg')
           },
           {
             name: '王五',
-            hobby: '烫头'
+            hobby: '烫头',
+            image: require('../static/cc.jpg')
           }
         ],
         /*
@@ -39,8 +43,15 @@ export default {
       }
       const obj = {
         data,
-        fileName: 'output',
-        fileUrl: `..${location.pathname}template.docx`
+        fileName: '文档',
+        fileUrl: `..${location.pathname}template.docx`,
+        forceHeight: 60, // 如果没传入setSize,默认图片高度60,宽度自适应
+        setSize (tagName) {
+          // 处理图片宽高方法
+          if (tagName === 'image') {
+            return [200, 300]
+          }
+        }
       }
       docxExport(obj)
     }
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,9 +2,11 @@ import Docxtemplater from 'docxtemplater'
 import { saveAs } from 'file-saver'
 import PizZip from 'pizzip'
 import * as PizZipUtils from 'pizzip/utils'
+import ImageModule from 'docxtemplater-image-module-free' // https://www.npmjs.com/package/docxtemplater-image-module-free
+
 /**
  * @method 生成word文件
- * @param params {} data模板数据, fileName输出文件名, fileUrl模板路径
+ * @param  Object data模板数据, fileName输出文件名, fileUrl模板路径,forceHeight 数字 图片高度固定,宽度自适应
  * @return Promise
  * @example
       const data = {
@@ -12,11 +14,40 @@ import * as PizZipUtils from 'pizzip/utils'
         last_name: 'Doe',
         phone: '0652455478',
         description: 'New Website'
+        avatar: require('../static/avatar.jpg'),
+        list: [
+          {
+            name: '张三',
+            hobby: '抽烟',
+            image: require('../static/aa.jpg')
+          },
+          {
+            name: '李四',
+            hobby: '喝酒',
+            image: require('../static/bb.jpg')
+          },
+          {
+            name: '王五',
+            hobby: '烫头',
+            image: require('../static/cc.jpg')
+          }
+        ],
+        address: {
+          city: 'London',
+          postal: 24324
+        }
       }
       const obj = {
         data,
-        fileName: 'output',
-        fileUrl: 'https://docxtemplater.com/tag-example.docx'
+        fileName: '文档',
+        fileUrl: `..${location.pathname}template.docx`,
+        forceHeight: 60, // 如果没传入setSize,默认图片高度60,宽度自适应
+        setSize (tagName) {
+          // 处理图片宽高方法
+          if (tagName === 'image') {
+            return [200, 300]
+          }
+        }
       }
       嵌套对象语法
       https://github.com/open-xml-templating/docxtemplater/issues/243
@@ -25,28 +56,75 @@ import * as PizZipUtils from 'pizzip/utils'
         console.log(res)
       })
  */
-export function docxExport (params) {
-  const { data, fileName, fileUrl } = params
+export function docxExport ({
+  data,
+  fileName,
+  fileUrl,
+  forceHeight = 100,
+  setSize
+}) {
   return new Promise((resolve, reject) => {
     PizZipUtils.getBinaryContent(fileUrl, (error, content) => {
       if (error) {
         reject(error)
         throw error
       }
-      var zip = new PizZip(content)
-      var doc = new Docxtemplater(zip, { linebreaks: true })
 
-      doc.setData(data)
+      const opts = {}
+      opts.centered = false // Set to true to always center images
 
-      doc.render()
+      // Pass your image loader
+      opts.getImage = function (tagValue, tagName) {
+        return new Promise(function (resolve, reject) {
+          PizZipUtils.getBinaryContent(tagValue, function (error, content) {
+            if (error) {
+              return reject(error)
+            }
+            return resolve(content)
+          })
+        })
+      }
 
-      var out = doc.getZip().generate({
-        type: 'blob',
-        mimeType:
-          'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+      // Pass the function that return image size
+      opts.getSize = function (img, tagValue, tagName) {
+        return new Promise(function (resolve, reject) {
+          const image = new Image()
+          image.src = tagValue
+          image.onload = function () {
+            const sizeArr = setSize(tagName)
+            if (sizeArr) {
+              resolve(sizeArr)
+            } else {
+              const ratio = forceHeight / image.height
+              const ratioWidth = Math.round(image.width * ratio)
+              resolve([ratioWidth, forceHeight])
+            }
+          }
+          image.onerror = function (e) {
+            console.log('img, tagValue, tagName : ', img, tagValue, tagName)
+            reject(e)
+          }
+        })
+      }
+
+      const imageModule = new ImageModule(opts)
+
+      const zip = new PizZip(content)
+
+      const doc = new Docxtemplater()
+        .loadZip(zip)
+        .attachModule(imageModule)
+        .compile()
+      doc.resolveData(data).then(function () {
+        doc.render()
+        const out = doc.getZip().generate({
+          type: 'blob',
+          mimeType:
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        })
+        saveAs(out, `${fileName}.docx`)
+        resolve(true)
       })
-      saveAs(out, `${fileName}.docx`)
-      resolve(true)
     })
   })
 }
